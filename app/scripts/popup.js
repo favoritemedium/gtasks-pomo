df04aef7608d199be26e2e1f03f6859cfdc7bb26
Tan Shiaw Uen <shiawuen@gmail.com>
2013-07-23 19:20:48 +0800

    wiring up popup listing

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,17 +1,41 @@
-/*globals Google, TasksList */
+/*globals Google, TasksList, TaskView */
 
 'use strict';
 
-Google.init(
-  '370558891874-tcji26dm7t571mgbl88phr42qdpbtluh.apps.googleusercontent.com'
-, 'x0wjP4psSLwuGeQyYtQ37lNg'
-, ['https://www.googleapis.com/auth/tasks']
-);
+(function(){
 
-Google.ready(function(){
+  /**
+   * Chrome runtime api
+   */
 
-  TasksList.list(function(err, items){
-    console.log(items);
+  var rt = chrome.runtime;
+
+  /**
+   * Tasks container
+   */
+
+  var tasks = document.querySelector('#tasks');
+
+  var showUI = function(err, items){
+    if (err) throw err;
+
+    items.forEach(function(task){
+      var view = new TaskView(task);
+      tasks.appendChild(view.el);
+    });
+  };
+
+  Google.init(
+    '370558891874-tcji26dm7t571mgbl88phr42qdpbtluh.apps.googleusercontent.com'
+  , 'x0wjP4psSLwuGeQyYtQ37lNg'
+  , ['https://www.googleapis.com/auth/tasks']
+  );
+
+  Google.ready(function(){
+    TasksList.list(function(err, taskslists){
+      // Take first taskslist from GTasks
+      taskslists[0].getItems(showUI);
+    });
   });
 
-});
+})();
