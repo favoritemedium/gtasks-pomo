0dbdad62e9563540e8da65f8ed30992a8c9e20d7
Tan Shiaw Uen <shiawuen@gmail.com>
2013-07-31 11:24:43 +0800

    remove unused files

--- app/scripts/base.js
+++
@@ -1,8 +0,0 @@
-/*global OAuth2 */
-
-var google = new OAuth2('google', {
-  client_id: '370558891874-tcji26dm7t571mgbl88phr42qdpbtluh.apps.googleusercontent.com',
-  client_secret: 'x0wjP4psSLwuGeQyYtQ37lNg',
-  api_scope: 'https://www.googleapis.com/auth/tasks'
-});
-
